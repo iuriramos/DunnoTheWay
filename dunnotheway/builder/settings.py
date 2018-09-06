7f2535652338337c7e21c902d6c2f264b6d05728
iuriramos <iuri.srb@gmail.com>
2018-09-05 23:21:50 -0300

    FIx spacing

--- dunnotheway/builder/settings.py
+++ dunnotheway/builder/settings.py
@@ -3,10 +3,12 @@ import os
 
 from common.settings import BASE_DIR
 
+
 # load config file
 CONFIG_PATH = os.path.join(BASE_DIR, 'builder', 'config.json')
 with open(CONFIG_PATH) as f:
     config = json.load(f)
+
 
 NUMBER_ENTRIES_PER_SECTION = config['NUMBER_ENTRIES_PER_SECTION'] # NUMBER OF POINTS PER SECTIONS 
 NUMBER_SECTIONS = config['NUMBER_SECTIONS'] # NUMBER OF SECTIONS TO BUILD REPORT
