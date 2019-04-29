4b437b555a5b0e56be7611662c649103e2b05e73
iuriramos <iuri.srb@gmail.com>
2019-04-29 16:01:32 -0300

    Add Intersection class

---
+++ dunnotheway/analyser/models/intersection.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+class Intersection:
+    '''
+    Class representing the intersection between a `convection cell` 
+    and airways connecting `departure_airport` and `destination_airport`.
+    '''
+
+    def __init__(self, convection_cell, departure_airport, destination_airport, impact):
+        self.convection_cell = convection_cell
+        self.departure_airport = departure_airport
+        self.destination_airport = destination_airport
+        self.impact = impact
+    
+
+class IntersectionManager:
+    '''
+    Class responsible for organizing the intersection between a `convection cell` 
+    and airways connecting `departure_airport` and `destination_airport`.
+    '''
+
+    def __init__(self):
+        self.airports_to_intersections = defaultdict(list)
+        # # convection cell might impact in more than one airway at the same time
+        # self.convection_cells_to_airways = defaultdict(list)
+
+    def set_intersection(self, intersection):
+        key = intersection.departure_airport, intersection.destination_airport
+        self.airports_to_intersections[key] = intersection
+
+
+    def get_intersections(self, departure_airport, destination_airport):
+        return self.airports_to_intersections[
+            departure_airport, destination_airport]
