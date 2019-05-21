e5ccbb64c80168963633f15d7aa2b44e0418d1f4
iuriramos <iuri.srb@gmail.com>
2019-05-21 17:36:38 -0300

    Test detector

--- dunnotheway/results/test_detector.py
+++ dunnotheway/results/test_detector.py
@@ -1,4 +1,5 @@
 import numpy as np
+import functools
 
 from common.db import open_database_session
 from engine.detector import search_intersections_convection_cells, ALGORITHM_MAP
@@ -7,10 +8,18 @@ from flight.models.flight_plan import FlightPlan
 from flight.models.flight_location import FlightLocation
 
 
-# TODO: change airport tracking list for performance reasons
-AIRPORT_TRACKING_LIST = None
+AIRPORT_TRACKING_LIST = [
+      ('SBRJ', 'SBSP'),
+    # ('SBSP', 'SBRJ'),
+    # ('SBBR', 'SBSP'),
+    # ('SBSP', 'SBBR'),
+    # ('SBFZ', 'SBGR'),
+    # ('SBBR', 'SBFZ'),
+
+    ('SBBR', 'SBRJ'),
+]
 ALGORITHM_NAMES = ['DBSCAN', 'HDBSCAN']
-MIN_ENTRIES_PER_SECTION_VALS = [10, 100, 1000]
+MIN_ENTRIES_PER_SECTION_VALS = [10, 50, 250]
 MIN_NUMBER_SAMPLES_VALS = [5, 25, 125]
 MAX_DISTANCE_BETWEEN_SAMPLES_VALS = [1000, 100, 10]
 
@@ -54,35 +63,42 @@ def gen_params():
 
 
 def get_normalization_results(manager, min_entries_per_section):
+    print('#' * 50)
+    print('Normalization Step')
+    print('#' * 50)
+    
     with open_database_session() as session:
         for departure_airport, destination_airport in manager:
             print ('#', departure_airport, destination_airport)
             print ('-' * 50)
             
-            for flight_plan in (FlightPlan.
-                flight_plans_from_airports(session, departure_airport, destination_airport)):
-
-                len_flight_positions = len(
-                    session.query(FlightLocation).filter(
+            count_flight_positions = sum(1 
+                for flight_plan in (FlightPlan.
+                    flight_plans_from_airports(session, departure_airport, destination_airport))
+                    for flight_location in session.query(FlightLocation).filter(
                         FlightLocation.flight.flight_plan == flight_plan))
-                
-                # (# positions) - (# positions')
-                sections = Section.sections_from_airports(
-                        departure_airport, 
-                        destination_airport, 
-                        min_entries_per_section=min_entries_per_section)
-                len_flight_positions_prime = sum(
-                    len(section) for section in sections)
+            
+            # (# positions) - (# positions')
+            sections = Section.sections_from_airports(
+                    departure_airport, 
+                    destination_airport, 
+                    min_entries_per_section=min_entries_per_section)
+            count_normalized_flight_positions = sum(
+                len(section) for section in sections)
 
-                # loss of information: positions -> positions'
-                print ('Loss', len_flight_positions_prime // len_flight_positions)
+            # ratio: positions -> positions'
+            print ('Normalization ratio - ', count_normalized_flight_positions // count_flight_positions)
 
-                # TODO(maybe) - PLOT airways in 3D
-                sections
+            # TODO(maybe) - PLOT airways in 3D
+            sections
 
 def get_delimitation_results(
     manager, algorithm_name, min_entries_per_section, 
     min_number_samples, max_distance_between_samples):
+    print('#' * 50)
+    print('Delimitation Step')
+    print('#' * 50)
+
     algorithm = ALGORITHM_MAP[algorithm_name]
 
     for departure_airport, destination_airport in manager:
@@ -97,26 +113,29 @@ def get_delimitation_results(
             max_distance_between_samples=max_distance_between_samples)
         
         # (# positions') - (# positions'')
-        len_flight_positions = np.array(
+        count_normalized_flight_positions = np.array(
             len(wrapper_section.section) for wrapper_section in wrapper_sections)
-        len_flight_positions_prime = np.array(
+        count_clusterized_flight_positions = np.array(
             len(wrapper_section) for wrapper_section in wrapper_sections)
 
-        # TODO(maybe) - PLOT diff distribution
-        diff_flight_positions = len_flight_positions - len_flight_positions_prime
-        print (diff_flight_positions)
+        # # TODO(maybe) - PLOT diff distribution
+        # diff_flight_positions = count_normalized_flight_positions - count_clusterized_flight_positions
+        # print (diff_flight_positions)
 
-        # average diff positions
-        print (diff_flight_positions.average())
+        # # average diff positions
+        # print (diff_flight_positions.average())
 
-        # TODO(maybe) - PLOT noise distribution
-        noise_flight_positions = diff_flight_positions / len_flight_positions
-        print (noise_flight_positions)
+        # # TODO(maybe) - PLOT noise distribution
+        # noise_flight_positions = diff_flight_positions / count_normalized_flight_positions
+        # print (noise_flight_positions)
 
-        # average diff positions
-        print (noise_flight_positions.average())
+        # # average diff positions
+        # print (noise_flight_positions.average())
 
-        # TODO(maybe) - PLOT sections and clusters
+        # ratio: positions' -> positions''
+        print ('Normalization ratio - ', count_clusterized_flight_positions.sum() // count_normalized_flight_positions.sum())
+
+        # TODO - PLOT sections and clusters
 
 
 def get_intersection_results(manager):
