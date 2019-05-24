25e516b45a002d99e00b78c491dbfd0c17ecc5b9
iuriramos <iuri.srb@gmail.com>
2019-05-24 19:02:25 -0300

    Refactor test_detector (output tables instead of stdout)

--- dunnotheway/engine/plot.py
+++ dunnotheway/engine/plot.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-from common.settings import BASE_DIR
 from common.db import open_database_session
 from engine.models.section import Section
 from flight.models.airport import Airport
@@ -16,30 +15,23 @@ from flight.models.airport import Airport
 from .settings import NUMBER_SECTIONS
 
 
-DATETIME_DIR_NAME, DIR_NAME, REPORTS_DIR = None, None, None
 SIZES = [200, 300, 400, 500, 600]
 COLORS = ['red', 'blue', 'green', 'orange', 'purple', 'black']
 
 
-def plot_sections(sections, number_sections=NUMBER_SECTIONS):
+def plot_sections(filepath, sections, number_sections=NUMBER_SECTIONS):
     '''Build cruising paths from departure airport to destination airport'''
-    global DATETIME_DIR_NAME, DIR_NAME, REPORTS_DIR
-    
-    DATETIME_DIR_NAME = datetime.today().strftime(r'%d-%m-%Y')
-    DIR_NAME = str(random.randrange(0, 100000))
-    REPORTS_DIR = os.path.join(BASE_DIR, 'results', 'reports', 
-            DATETIME_DIR_NAME, DIR_NAME)
-    
+
     def sections_subset(sections):
         '''Return at most `NUMBER_SECTIONS` sections'''
         step = max(1, len(sections)//number_sections)
         return sections[::step]
 
     for section in sections_subset(sections):
-        plot_section(section)
+        plot_section(filepath, section)
 
 
-def plot_section(section):
+def plot_section(filepath, section):
     '''Plot flight records with their respective labels'''
     flight_locations, labels = section.flight_locations, section.labels
     
@@ -69,19 +61,13 @@ def plot_section(section):
     axis.scatter(latitudes_longitudes, altitudes, c=labels, alpha=0.5)
 
     # save results in a figure    
-    filepath = _get_filepath_from_section(section)
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    
+    if longitude_based:
+        filename = str(first_fl.longitude)
+    else:
+        filename = str(first_fl.latitude)
+    
+    filepath = os.path.join(filepath, filename + '.pdf')
     plt.savefig(filepath)
-
-def _get_filepath_from_section(section):
-    '''Return file path for section report'''
-    first_fl, last_fl = section.flight_locations[0], section.flight_locations[-1]
-    flight_plan = first_fl.flight.flight_plan
-    subdir_name =  '-'.join([
-        flight_plan.departure_airport.icao_code, 
-        flight_plan.destination_airport.icao_code])
-    subdir_path = os.path.join(REPORTS_DIR, subdir_name)
-    if not os.path.exists(subdir_path):
-        os.makedirs(subdir_path)
-    filename = (str(first_fl.longitude) +'_lon' if first_fl.longitude == last_fl.longitude
-        else str(first_fl.latitude) + '_lat') + '.pdf'
-    return os.path.join(subdir_path, filename)
--- dunnotheway/results/test_detector.py
+++ dunnotheway/results/test_detector.py
@@ -1,13 +1,14 @@
 import sys
 sys.path.append('/home/iuri/workspace/dunnotheway/dunnotheway')
 
-
+import os
 import functools
 import operator
 import numpy as np
 import pandas as pd
 from scipy import stats
 
+from common.settings import BASE_DIR
 from common.db import open_database_session
 from engine.detector import search_intersections_convection_cells, ALGORITHM_MAP
 from engine.plot import plot_sections
@@ -15,7 +16,10 @@ from engine.models.section import Section
 from flight.models.flight import Flight
 from flight.models.flight_plan import FlightPlan
 from flight.models.flight_location import FlightLocation
+from weather.models.convection_cell import ConvectionCell
 
+
+REPORTS_DIR = os.path.join(BASE_DIR, 'results', 'reports')
 
 AIRPORT_TRACKING_LIST = [
       ('SBRJ', 'SBSP'),
@@ -34,39 +38,44 @@ MAX_DISTANCE_BETWEEN_SAMPLES_VALS = [1000, 100, 10]
 
 
 def main():
-    for (algorithm_name, 
-        min_entries_per_section, 
-        min_number_samples, 
-        max_distance_between_samples) in gen_params():
-        print('*' * 120)
-        print('Parameters Setup')
-        print (
-            '# algorithm_name =', algorithm_name, 
-            ', min_entries_per_section =', min_entries_per_section, 
-            ', min_number_samples =', min_number_samples, 
-            ', max_distance_between_samples =', max_distance_between_samples)
-        print('*' * 120)
-        
-        manager = search_intersections_convection_cells(
-            airport_tracking_list=AIRPORT_TRACKING_LIST,
-            algorithm_name=algorithm_name,
-            min_entries_per_section=min_entries_per_section, 
-            min_number_samples=min_number_samples, 
-            max_distance_between_samples=max_distance_between_samples)
+    for departure_destination_airports in AIRPORT_TRACKING_LIST:
+        filepath = os.path.join(REPORTS_DIR, str(departure_destination_airports))
 
-        # normalization results
-        get_normalization_results(manager, min_entries_per_section)
+        for (algorithm_name, 
+            min_entries_per_section, 
+            min_number_samples, 
+            max_distance_between_samples) in gen_params():
 
-        # delimitation results
-        get_delimitation_results(
-            manager, 
-            algorithm_name=algorithm_name,
-            min_entries_per_section=min_entries_per_section, 
-            min_number_samples=min_number_samples, 
-            max_distance_between_samples=max_distance_between_samples)
+            filepath = build_filepath_from_params(
+                filepath,
+                algorithm_name, 
+                min_entries_per_section, 
+                min_number_samples, 
+                max_distance_between_samples)
+            if not os.path.exists(filepath):
+                os.makedirs(filepath)
+            
+            manager = search_intersections_convection_cells(
+                airport_tracking_list=[departure_destination_airports],
+                algorithm_name=algorithm_name,
+                min_entries_per_section=min_entries_per_section, 
+                min_number_samples=min_number_samples, 
+                max_distance_between_samples=max_distance_between_samples)
 
-        # intersection results
-        get_intersection_results(manager)
+            # normalization results
+            get_normalization_results(filepath, manager, min_entries_per_section)
+
+            # delimitation results
+            get_delimitation_results(
+                filepath,
+                manager, 
+                algorithm_name=algorithm_name,
+                min_entries_per_section=min_entries_per_section, 
+                min_number_samples=min_number_samples, 
+                max_distance_between_samples=max_distance_between_samples)
+
+            # intersection results
+            get_intersection_results(filepath, manager)
 
 
 def gen_params():
@@ -79,17 +88,27 @@ def gen_params():
                     yield algorithm_name, min_entries_per_section, min_number_samples, max_distance_between_samples
 
 
+def build_filepath_from_params(
+    filepath,
+    algorithm_name, 
+    min_entries_per_section, 
+    min_number_samples, 
+    max_distance_between_samples
+):
+    return os.path.join(
+        filepath, 
+        algorithm_name,
+        '_min_entries_per_section_' + min_entries_per_section +
+        '_min_number_samples_' + min_number_samples +
+        '_max_distance_between_samples_' + max_distance_between_samples,
+    )
 
-def get_normalization_results(manager, min_entries_per_section):
-    print('#' * 50)
-    print('Normalization Step')
-    print('#' * 50)
+
+def get_normalization_results(filepath, manager, min_entries_per_section):
     
     with open_database_session() as session:
         for departure_airport, destination_airport in manager:
-            print ('#', departure_airport, destination_airport)
-            print ('-' * 50)
-            
+
             count_flight_positions = sum(1 
                 for flight_plan in (FlightPlan.
                     flight_plans_from_airports(session, departure_airport, destination_airport))
@@ -105,24 +124,27 @@ def get_normalization_results(manager, min_entries_per_section):
                 len(section) for section in sections)
 
             # ratio: positions -> positions'
-            print ('Normalization ratio:', 
-                format(count_normalized_flight_positions / count_flight_positions, '.3f'))
+            ratio = float(format(count_normalized_flight_positions / count_flight_positions, '.3f'))
+
+            df = pd.DataFrame(
+                data={
+                    'Posições de voo': [count_flight_positions], 
+                    'Posições de voo normalizadas': [count_normalized_flight_positions],
+                    'Proporção': [ratio],
+                }
+            )
+
+            df.to_csv(path_or_buf=os.path.join(filepath, 'normalization.csv', index=False))
 
 
 def get_delimitation_results(
+    filepath,
     manager, algorithm_name, min_entries_per_section, 
     min_number_samples, max_distance_between_samples):
-    print()
-    print('#' * 50)
-    print('Delimitation Step')
-    print('#' * 50)
-
     algorithm = ALGORITHM_MAP[algorithm_name]
 
     for departure_airport, destination_airport in manager:
-        print ('#', departure_airport, destination_airport)
-        print ('-' * 50)
-
+        
         wrapper_sections = algorithm.sections_from_airports(
             departure_airport, 
             destination_airport, 
@@ -136,29 +158,34 @@ def get_delimitation_results(
         count_clusterized_flight_positions = np.array([
             len(wrapper_section) for wrapper_section in wrapper_sections])
 
-        # # TODO(maybe) - PLOT diff distribution
-        # diff_flight_positions = count_normalized_flight_positions - count_clusterized_flight_positions
-        # print (diff_flight_positions)
+        # diff: positions' -> positions''
+        diff = count_normalized_flight_positions - count_clusterized_flight_positions
 
-        # # average diff positions
-        # print (diff_flight_positions.average())
+        df = pd.DataFrame(
+            data={
+                'Posições de voo normalizadas': count_normalized_flight_positions, 
+                'Posições de voo pertencentes a clusters': count_clusterized_flight_positions,
+                'Diferença': diff,
+            }
+        )
 
-        # # TODO(maybe) - PLOT noise distribution
-        # noise_flight_positions = diff_flight_positions / count_normalized_flight_positions
-        # print (noise_flight_positions)
+        # summary delimitation table
+        (df.describe().transpose()
+            .to_csv(path_or_buf=os.path.join(filepath, 'delimitation_summary.csv')))
+        
+        # full delimitation table
+        df.loc['Total',:]= df.sum(axis=0)
+        df.to_csv(path_or_buf=os.path.join(filepath, 'delimitation.csv'))
 
-        # # average diff positions
-        # print (noise_flight_positions.average())
-
-        # ratio: positions' -> positions''
-        print ('Delimitation Ratio:', 
-            format(count_clusterized_flight_positions.sum() / count_normalized_flight_positions.sum(), '.2f'))
-        print()
-
-        # Labels Metrics
+        # labels Metrics
         labels = np.array([
             len(set(wrapper_section.labels)) -1 for wrapper_section in wrapper_sections])
         
+        # summary clusters table (regular metrics)
+        df = pd.DataFrame(data={'Clusters': labels})
+        (df.describe().transpose()
+            .to_csv(path_or_buf=os.path.join(filepath, 'clusters_summary_1.csv')))
+                
         mean = np.mean(labels)
         median = np.median(labels)
         mode = stats.mode(labels)
@@ -167,43 +194,63 @@ def get_delimitation_results(
         variance = np.var(labels)
         standard_deviation = np.std(labels)
 
-        print('Labels Mean:', format(mean, '.3f'))
-        print('Labels Median:', format(median, '.3f'))
-        print('Labels Mode:', mode)
-        print('Labels Range:', format(range_, '.3f'))
-        print('Labels IQR:', format(iqr, '.3f'))
-        print('Labels Variance:', format(variance, '.3f'))
-        print('Labels Standard Variation:', format(standard_deviation, '.3f'))
+        # summary clusters table (chosen metrics)
+        df = pd.DataFrame(
+            data={
+                'Média': [float(format(mean, '.3f'))], 
+                'Mediana': [float(format(median, '.3f'))], 
+                'Moda': [str(mode)], 
+                'Variação': [float(format(range_, '.3f'))], 
+                'IQR': [float(format(iqr, '.3f'))], 
+                'Variância': [float(format(variance, '.3f'))], 
+                'Desvio Padrão': [float(format(standard_deviation, '.3f'))], 
+            }
+        )
+        df.to_csv(path_or_buf=os.path.join(filepath, 'clusters_summary_2.csv'))
 
         # PLOT sections 
-        plot_sections(wrapper_sections, number_sections=-1) # plot ALL sections
+        plot_sections(
+            filepath=os.path.join(filepath, 'imgs'),
+            sections=wrapper_sections, 
+            number_sections=-1) # plot ALL sections
 
 
-def get_intersection_results(manager):
-    print()
-    print('#' * 50)
-    print('Intersections Step')
-    print('#' * 50)
+def get_intersection_results(filepath, manager):
 
     for (departure_airport, destination_airport), intersections in manager.items():
-        print ('#', departure_airport, destination_airport)
-        print ('-' * 50)
 
-        print ('Cell', 'Impact', sep=' | ')
-        for cell, _, _, impact in intersections:
-            print (cell, impact, sep=' | ')
+        # convection cells within departure and destination airports
+        with open_database_session() as session:
+            cells = [cell for cell in ConvectionCell.all_convection_cells(session) 
+                if cell.is_convection_cells_between_airports(departure_airport, destination_airport)]
 
-        print()
-        print ('Most impactful convection cells (top 10)')
-        top_impactful_cells = sorted(
-            ((impact, cell) for cell, _, _, impact in intersections), reverse=True)[:10]
-        print ('Cell', 'Impact', sep=' | ')
-        for cell, impact in top_impactful_cells:
-            print (cell, impact, sep=' | ')
+        intersected_cells = [cell for cell, _, _, impact in intersections]
 
-        max_impact, max_cell = max((impact, cell) for cell, _, _, impact in intersections)
-        print()
-        print ('Most impactful convection cell: ', max_cell, ' | ', max_impact)
+        df = pd.DataFrame(
+            data={
+                'Células convectivas': [len(cells)], 
+                'Células convectivas intersectadas': [len(intersected_cells)], 
+            }
+        )
+        df.to_csv(path_or_buf=os.path.join(filepath, 'cells.csv'), index=False)
+
+        impacts = [impact for cell, _, _, impact in intersections]
+        latitudes = [lat for lat, _, _ in intersected_cells]
+        longitudes = [lon for _, lon, _ in intersected_cells]
+        radiuses = [rad for _, _, rad in intersected_cells]
+        timestamps = [cell.timestamp.strftime(r'%d-%m-%Y %H:%M') for cell in intersected_cells]
+
+        df = pd.DataFrame(
+            data={
+                'Impacto': impacts, 
+                'Latitude': latitudes, 
+                'Longitude': longitudes, 
+                'Raio': radiuses, 
+                'Timestamp': timestamps, 
+            }
+        )
+        (df.sort_values(by=['Impacto'], ascending=False)
+            .to_csv(path_or_buf=os.path.join(filepath, 'intersection.csv')))
 
 
 if __name__ == "__main__":
