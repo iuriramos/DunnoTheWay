b1274c714657e5184c69e88295083f1586fbc6e4
iuriramos <iuri.srb@gmail.com>
2019-05-24 14:34:22 -0300

    Refactor detactor script

--- dunnotheway/results/test_detector.py
+++ dunnotheway/results/test_detector.py
@@ -2,8 +2,11 @@ import sys
 sys.path.append('/home/iuri/workspace/dunnotheway/dunnotheway')
 
 
-import numpy as np
 import functools
+import operator
+import numpy as np
+import pandas as pd
+from scipy import stats
 
 from common.db import open_database_session
 from engine.detector import search_intersections_convection_cells, ALGORITHM_MAP
@@ -35,6 +38,15 @@ def main():
         min_entries_per_section, 
         min_number_samples, 
         max_distance_between_samples) in gen_params():
+        print('*' * 120)
+        print('Parameters Setup')
+        print (
+            '# algorithm_name =', algorithm_name, 
+            ', min_entries_per_section =', min_entries_per_section, 
+            ', min_number_samples =', min_number_samples, 
+            ', max_distance_between_samples =', max_distance_between_samples)
+        print('*' * 120)
+        
         manager = search_intersections_convection_cells(
             airport_tracking_list=AIRPORT_TRACKING_LIST,
             algorithm_name=algorithm_name,
@@ -93,16 +105,14 @@ def get_normalization_results(manager, min_entries_per_section):
                 len(section) for section in sections)
 
             # ratio: positions -> positions'
-            print ('Normalization ratio - ', 
-                count_normalized_flight_positions // count_flight_positions)
-
-            # TODO(maybe) - PLOT airways in 3D
-            sections
+            print ('Normalization ratio:', 
+                format(count_normalized_flight_positions / count_flight_positions, '.3f'))
 
 
 def get_delimitation_results(
     manager, algorithm_name, min_entries_per_section, 
     min_number_samples, max_distance_between_samples):
+    print()
     print('#' * 50)
     print('Delimitation Step')
     print('#' * 50)
@@ -121,10 +131,10 @@ def get_delimitation_results(
             max_distance_between_samples=max_distance_between_samples)
         
         # (# positions') - (# positions'')
-        count_normalized_flight_positions = np.array(
-            len(wrapper_section.section) for wrapper_section in wrapper_sections)
-        count_clusterized_flight_positions = np.array(
-            len(wrapper_section) for wrapper_section in wrapper_sections)
+        count_normalized_flight_positions = np.array([
+            len(wrapper_section.section) for wrapper_section in wrapper_sections])
+        count_clusterized_flight_positions = np.array([
+            len(wrapper_section) for wrapper_section in wrapper_sections])
 
         # # TODO(maybe) - PLOT diff distribution
         # diff_flight_positions = count_normalized_flight_positions - count_clusterized_flight_positions
@@ -141,14 +151,36 @@ def get_delimitation_results(
         # print (noise_flight_positions.average())
 
         # ratio: positions' -> positions''
-        print ('Normalization ratio - ', 
-            count_clusterized_flight_positions.sum() // count_normalized_flight_positions.sum())
+        print ('Delimitation Ratio:', 
+            format(count_clusterized_flight_positions.sum() / count_normalized_flight_positions.sum(), '.2f'))
+        print()
+
+        # Labels Metrics
+        labels = np.array([
+            len(set(wrapper_section.labels)) -1 for wrapper_section in wrapper_sections])
+        
+        mean = np.mean(labels)
+        median = np.median(labels)
+        mode = stats.mode(labels)
+        range_ = (labels.max() - labels.min())
+        iqr = stats.iqr(labels)
+        variance = np.var(labels)
+        standard_deviation = np.std(labels)
+
+        print('Labels Mean:', format(mean, '.3f'))
+        print('Labels Median:', format(median, '.3f'))
+        print('Labels Mode:', mode)
+        print('Labels Range:', format(range_, '.3f'))
+        print('Labels IQR:', format(iqr, '.3f'))
+        print('Labels Variance:', format(variance, '.3f'))
+        print('Labels Standard Variation:', format(standard_deviation, '.3f'))
 
         # PLOT sections 
         plot_sections(wrapper_sections, number_sections=-1) # plot ALL sections
 
 
 def get_intersection_results(manager):
+    print()
     print('#' * 50)
     print('Intersections Step')
     print('#' * 50)
@@ -161,8 +193,17 @@ def get_intersection_results(manager):
         for cell, _, _, impact in intersections:
             print (cell, impact, sep=' | ')
 
-        impact, cell = max((impact, cell) for cell, _, _, impact in intersections)
-        print ('Most impactful convection cell: ', cell, ' | ', impact)
+        print()
+        print ('Most impactful convection cells (top 10)')
+        top_impactful_cells = sorted(
+            ((impact, cell) for cell, _, _, impact in intersections), reverse=True)[:10]
+        print ('Cell', 'Impact', sep=' | ')
+        for cell, impact in top_impactful_cells:
+            print (cell, impact, sep=' | ')
+
+        max_impact, max_cell = max((impact, cell) for cell, _, _, impact in intersections)
+        print()
+        print ('Most impactful convection cell: ', max_cell, ' | ', max_impact)
 
 
 if __name__ == "__main__":
